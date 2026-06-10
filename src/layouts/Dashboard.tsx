336d4bd4fd63db82dc314b2d63c090b8d7a7785b
akshayaj108 <akshayjadhao806@gmail.com>
2026-06-10 21:23:49 +0530

    fix active meu color issue

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -82,7 +82,7 @@ const Dashboard = () => {
           </div>
           <Menu
             theme="light"
-            defaultSelectedKeys={["/"]}
+            defaultSelectedKeys={[location.pathname]}
             mode="inline"
             items={items}
           />
