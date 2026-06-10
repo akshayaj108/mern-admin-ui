be7921ef3e929de79fb7e6e6401f09b00e86e165
akshayaj108 <akshayjadhao806@gmail.com>
2026-06-10 21:19:02 +0530

    fix navigation url redirection issue

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Navigate, NavLink, Outlet } from "react-router-dom";
+import { Navigate, NavLink, Outlet, useLocation } from "react-router-dom";
 import { useAuthStore } from "../store";
 import { useState } from "react";
 import Icon, { BellFilled, UserOutlined } from "@ant-design/icons";
@@ -56,6 +56,7 @@ const getMenuItems = (role: string) => {
 };
 
 const Dashboard = () => {
+  const location = useLocation();
   const { logoutUser } = useLogout();
   const [collapsed, setCollapsed] = useState(false);
   const {
@@ -64,7 +65,7 @@ const Dashboard = () => {
 
   const { user } = useAuthStore();
   if (user === null) {
-    return <Navigate to={"/auth/login"} replace={true} />;
+    return <Navigate to={`/auth/login?returnTo=${location.pathname}`} replace={true} />;
   }
   const items = getMenuItems(user?.role);
   return (
--- src/layouts/NonAuthBoard.tsx
+++ src/layouts/NonAuthBoard.tsx
@@ -1,11 +1,13 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuthStore } from "../store"
 
 
 const NonAuthBoard = () => {
+  const location = useLocation();
     const { user } = useAuthStore();
     if(user !== null){
-        return <Navigate to={'/'} replace={true} />
+        const returnPath = new URLSearchParams(location.search).get('returnTo') || '/'
+        return <Navigate to={returnPath} replace={true} />
     }
   return (
    <>
